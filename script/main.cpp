e7802d81f4c123e5a3a3faf8406a096e70c19485
abinalexpothen <abinalexpothen@gmail.com>
2021-07-25 00:20:46 -0400

    Added state prediction.

--- script/main.cpp
+++ script/main.cpp
@@ -15,9 +15,21 @@ std::tuple<double, double> measurement_update(double mean1, double variance1,
   return std::make_tuple(new_mean, new_variance);
 }
 
+std::tuple<double, double> state_prediction(double mean1, double variance1,
+                                            double mean2, double variance2) {
+  double new_mean = mean1 + mean2;
+  double new_variance = variance1 + variance2;
+  return std::make_tuple(new_mean, new_variance);
+}
+
 int main() {
   double new_mean, new_variance;
   std::tie(new_mean, new_variance) = measurement_update(20.0, 5.0, 30.0, 5.0);
-  printf("new_mean: %f, new_variance: %f", new_mean, new_variance);
+  printf("\n Measurement update: new_mean: %f, new_variance: %f", new_mean,
+         new_variance);
+  std::tie(new_mean, new_variance) = state_prediction(10.0, 4.0, 12.0, 4.0);
+  printf("\n State update: new_mean: %f, new_variance: %f", new_mean,
+         new_variance);
+
   return 0;
 }
